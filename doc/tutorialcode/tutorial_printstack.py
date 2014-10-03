6ca9eb4453408c3f74ced11b8430addc30d799ab
PhilippBoeing <philipp.boeing@gmail.com>
2014-10-03 14:28:42 +0200

    tutorial code files in documentation path update

---
+++ doc/tutorialcode/tutorial_printstack.py
@@ -0,0 +1,34 @@
+from aosb import *
+
+class CodingGFP(Circuit):
+    def mainCircuit(self):
+        declareNewMolecule('GFP') 
+        self.addPart(Promoter)
+        self.addPart(CodingRegion(GFP))
+        
+class DesignRules(Aspect):
+    def mainAspect(self):
+        anyCodingRegion = PartSignature('*.CodingRegion+')
+        
+        beforeCodingRegion = PointCut(anyCodingRegion,PointCut.BEFORE)
+        afterCodingRegion = PointCut(anyCodingRegion,PointCut.AFTER)
+        
+        self.addAdvice(beforeCodingRegion,self.insertRBS)
+        self.addAdvice(afterCodingRegion,self.insertTerminator)
+    
+    def insertRBS(self,context):
+        self.addPart(RBS)
+    
+    def insertTerminator(self,context):
+        self.addPart(Terminator)
+        
+class PrintStack(Aspect):
+    def mainAspect(self):
+        self.addAdvice(PointCut('*.*',PointCut.AFTER),self.printStack)
+    
+    def printStack(self,context):
+        print ','.join(str(context.within[i]) for i in range(len(context.within))) + " add: " + str(context.part)
+    
+
+compiledDesign = Weaver(CodingGFP,DesignRules,PrintStack).output()
+print compiledDesign
---
+++ doc/tutorialcode/tutorial_weaveroutput.py
@@ -0,0 +1,40 @@
+from aosb import *
+
+class CodingGFP(Circuit):
+    def mainCircuit(self):
+        declareNewMolecule('GFP') 
+        self.addPart(Promoter)
+        self.addPart(CodingRegion(GFP))
+        
+class DesignRules(Aspect):
+    def mainAspect(self):
+        anyCodingRegion = PartSignature('*.CodingRegion+')
+        
+        beforeCodingRegion = PointCut(anyCodingRegion,PointCut.BEFORE)
+        afterCodingRegion = PointCut(anyCodingRegion,PointCut.AFTER)
+
+        self.addTypeAdvice(PartSignature('*.Terminator'),self.terminatorRuleString,'terminatorRuleString')
+        
+        self.addAdvice(beforeCodingRegion,self.insertRBS)
+        self.addAdvice(afterCodingRegion,self.insertTerminator)
+
+    def terminatorRuleString(self,part):
+        print 'hello\n'
+        for before in part.before:
+            print before
+
+    def insertRBS(self,context):
+        self.addPart(RBS)
+    
+    def insertTerminator(self,context):
+        self.addPart(Terminator)
+        
+class NumberOfPartsAspect(Aspect):
+    def mainAspect(self):
+        self.addWeaverOutput(self.printNumberOfParts)
+        
+    def printNumberOfParts(self,weaverOutput):
+        print "The design has "+str(len(weaverOutput.partList))+" parts."
+
+compiledDesign = Weaver(CodingGFP,DesignRules,NumberOfPartsAspect).output()
+compiledDesign.printNumberOfParts()
