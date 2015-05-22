2efb57c21150dc45c2117976dd31990b00530783
cpbarnes <christopher.barnes@ucl.ac.uk>
2015-05-22 14:26:06 +0100

    More variation on DesignRules

--- synbioweaver/examples/tutorial_designrules_robust2.py
+++ synbioweaver/examples/tutorial_designrules_robust2.py
@@ -7,39 +7,40 @@ class CodingGFP(Circuit):
         self.addPart(Promoter)
         #self.addPart(RBS)
         self.addPart(CodingRegion(GFP))
-        self.addPart(Terminator)
+        #self.addPart(Terminator)
         
-class DesignRulesRobust(Aspect):
+class DesignRules(Aspect):
     def mainAspect(self):
-        # match any coding region
         anyCodingRegion = PartSignature('*.CodingRegion+')
-
-        # match any terminator 
-        anyTerminator = PartSignature("*.Terminator+")
-
-        # add a terminator after every coding region
-        # then examine terminators to see if there are two adjacent
+        
         beforeCodingRegion = PointCut(anyCodingRegion,PointCut.BEFORE)
         afterCodingRegion = PointCut(anyCodingRegion,PointCut.AFTER)
-        insteadOfTerminator = PointCut(anyTerminator,PointCut.REPLACE)
-
+        
         self.addAdvice(beforeCodingRegion,self.insertRBS)
         self.addAdvice(afterCodingRegion,self.insertTerminator)
-        self.addAdvice(insteadOfTerminator,self.replaceTerminator)
-
+    
     def insertRBS(self,context):
-        # Check to see if any of the previous parts are RBS. If not add an RBS
-        if all( isinstance(x,RBS) == False for x in context.part.before):
-            self.addPart(RBS)
-
+        self.addPart(RBS)
+    
     def insertTerminator(self,context):
         self.addPart(Terminator)
 
+
+class CheckParts(Aspect):
+    def mainAspect(self):
+        # match any terminator that has been added by DesignRules
+        addedTerminator = PartSignature("DesignRules.Terminator+")
+
+        insteadOfTerminator = PointCut(addedTerminator,PointCut.REPLACE)
+
+        self.addAdvice(insteadOfTerminator,self.replaceTerminator)
+
     def replaceTerminator(self,context):
+        print "HERE:"
         if all( isinstance(x,Terminator) == False for x in context.part.before):
             return False #in this case, don't replace the terminator
         else:
             return True #in this case, replace the part with nothing
 
-compiledDesign = Weaver(CodingGFP,DesignRulesRobust).output()
+compiledDesign = Weaver(CodingGFP,DesignRules,CheckParts).output()
 print compiledDesign
