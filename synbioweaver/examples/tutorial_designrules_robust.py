f0c3ad5e2f8d5c2bbb955233e9aa3c352395d4ef
cpbarnes <christopher.barnes@ucl.ac.uk>
2015-05-22 13:56:00 +0100

    Added robust version of DesignRules

--- synbioweaver/examples/tutorial_designrules_robust.py
+++ synbioweaver/examples/tutorial_designrules_robust.py
@@ -4,45 +4,28 @@ declareNewMolecule('GFP')
 
 class CodingGFP(Circuit):
     def mainCircuit(self):
-
-        #self.addPart(Promoter)
+        self.addPart(Promoter)
         #self.addPart(RBS)
-        #self.addPart(CodingRegion(GFP))
-        #self.addPart(Terminator)
-
-        #self.addPart(Promoter)
-        ##self.addPart(RBS)
-        #self.addPart(CodingRegion(GFP))
-        #self.addPart(Terminator)
-
-        self.addPart(Promoter)
-        self.addPart(RBS)
         self.addPart(CodingRegion(GFP))
-        self.addPart(Terminator)
-
-        self.addPart(Promoter)
-        ##self.addPart(RBS)
-        #self.addPart(CodingRegion(GFP))
-        ##self.addPart(Terminator)
+        #self.addPart(Terminator)
         
 class DesignRulesRobust(Aspect):
     def mainAspect(self):
+        # match any coding region
         anyCodingRegion = PartSignature('*.CodingRegion+')
-        anyTerminator = PartSignature('*.Terminator+')
-        #unTerminatedCodingRegion =  PartSignature('*.CodingRegion+') % PointCutExpressionNot( PartSignature('*.Terminator+') )
 
+        # match any terminator 
+        addedTerminator = PartSignature("*.Terminator+")
 
+        # add a terminator after every coding region
+        # then examine terminators to see if there are two adjacent
         beforeCodingRegion = PointCut(anyCodingRegion,PointCut.BEFORE)
         afterCodingRegion = PointCut(anyCodingRegion,PointCut.AFTER)
-        insteadOfTerminator = PointCut(PartSignature("!DesignRulesAspect.Terminator+"),PointCut.REPLACE)
+        insteadOfTerminator = PointCut(addedTerminator,PointCut.REPLACE)
 
         self.addAdvice(beforeCodingRegion,self.insertRBS)
         self.addAdvice(afterCodingRegion,self.insertTerminator)
         self.addAdvice(insteadOfTerminator,self.replaceTerminator)
-
-    def printInfo(self,context):
-        print "\tPRINT INFO::", context.part
-
 
     def insertRBS(self,context):
         # Check to see if any of the previous parts are RBS. If not add an RBS
@@ -53,6 +36,7 @@ class DesignRulesRobust(Aspect):
         self.addPart(Terminator)
 
     def replaceTerminator(self,context):
+        print "HERE:"
         if all( isinstance(x,Terminator) == False for x in context.part.before):
             return False #in this case, don't replace the terminator
         else:
