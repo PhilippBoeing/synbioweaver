db265ed6bd0339704b825f43fe771c58a5d01da7
cpbarnes <christopher.barnes@ucl.ac.uk>
2018-07-25 14:18:27 +0100

    added run.sh for clear setups

---
+++ examples/rule-based-model/run.sh
@@ -0,0 +1,9 @@
+# location of synbioweaver
+export PYTHONPATH=../../
+
+# location of KaSim v3.5 downloadable from here:
+# https://github.com/Kappa-Dev/KaSim/releases
+# note that pysb assumes that it is named KaSim
+export PATH=$PATH:/usr/local/share
+
+python run_rulesRepressilator.py 
